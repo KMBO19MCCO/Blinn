eb8ac6477d333fd69192a87a13f8cc6b8b63bbf3
Ian Gramovich <grrramovich@yandex.ru>
2022-12-15 19:05:40 +0300

    Begin of general case

--- main.cpp
+++ main.cpp
@@ -2,98 +2,196 @@
 #include <vector>
 #include <cmath> 
 #include <excerpt/excerpt.h>
+#include <Eigen/Dense>
 
+//---Aliases---
+template<typename T>
+using Vector4T = Eigen::Matrix<T, 4, 1>;
+
+template<typename T>
+using Matrix2T = Eigen::Matrix<T, 2, 2>;
+
+template<typename T>
+using Matrix4T = Eigen::Matrix<T, 4, 4>;
+//-------------
 
 template <typename T>
-bool is_equal(T a, T b)
+class Blinn
 {
-    return fabs(a - b) <= ( ( fabs(a) > fabs(b) ? fabs(b) : fabs(a) ) * std::numeric_limits<T>::epsilon());
-}
-
-template <typename T>
-std::vector<std::vector<T>> blinn(T A, T B, T C, T D)
-{
-    std::vector<std::vector<T>> roots;
-    B /= A;
-    C /= A;
-    D /= A;
-    A  = 1;
-    
-    B /= 3;
-    C /= 3;
-    
-    T d1 = pr_product_difference<T>(A, C, B, B);
-    T d2 = pr_product_difference<T>(A, D, B, C);
-    T d3 = pr_product_difference<T>(B, D, C, C);
-    T det = pr_product_difference<T>(4.0 * d1, d3, d2, d2);
-    
-    if (det <= 0)
+public:
+    Blinn(T A, T B, T C, T D, T t, T u, T k): m_A(A), m_B(B), m_C(C), m_D(D), m_t(t), m_u(u), m_k(k)
     {
-        T At, Cbar, Dbar;
-        if (pow(B, 3) * D >= pow(C, 3) * A)
-        {
-            At = A;
-            Cbar = d1;
-            Dbar = pr_product_difference<T>(A, d2, 2 * B, d1);
-        }
-        else
-        {
-            At = D;
-            Cbar = d3;
-            Dbar = pr_product_difference<T>(2 * C, d3, D, d2);
-        }
+        //calc square of params
+        m_t2 = std::pow(m_t, 2);
+        m_u2 = std::pow(m_u, 2);
         
-        //copysign(a, b) = sign(b) * a (from <cmath>)
-        //cbrt(x) like pow(x, 1.0/3.0), but more accurate
-        T T0 = -copysign(fabs(At) * sqrt(-det), Dbar);
-        T T1 = -Dbar + T0;
-        T p = cbrt(T1 / 2.0);
-        T q = is_equal<T>(T0, T1) ? (-p) : (-Cbar / p);
-        T xt1 = (Cbar <= 0) ? (p + q) : (-Dbar / (fma(p,p, fma(q,q, Cbar))));
+        //calc s and v
+        auto tmp = [](T X, T Y, T Z) 
+                        {return k * (pr_product_difference<T>(t2, X, -u2, Z) + (2 * t * u * Y))};
         
-        if (pow(B, 3) * D >= pow(C, 3) * A)
-        {
-            roots.push_back({xt1 - B, A});
-        }
-        else
-        {
-            roots.push_back({-D, xt1 + C});
-        }
-                                            
+        m_s = -tmp(B, C, D);
+        m_v = tmp(A, B, C);
+        
+        //calc square of params
+        m_s2 = std::pow(m_s, 2);
+        m_v2 = std::pow(m_v, 2);
+        
+        
+        //---first covariant---
+        first_covariant();
+        
+        //---second covariant---
+        second_covariant();
+        
+        //---Third covariant---
+        third_covariant();
+        
+    } 
+    
+    ~Blinn(){}
+    
+    int solve(vector<fp_t> &roots)
+    {
+        //soon...
     }
+
+private:
+    //coefs of polynom
+    T m_A;
+    T m_B;
+    T m_C;
+    T m_D;
+    
+    //params
+    T m_t;
+    T m_u;
+    T m_k;
+    T m_s;
+    T m_v;
+    
+    //frequently used degrees
+    T m_t2;
+    T m_u2;
+    T m_s2;
+    T m_v2;
+    
+    //Result of first covariant
+    Vector4T<T> m_coefs;
+    
+    //Result of second covariant
+    T m_delta1;
+    T m_delta2;
+    T m_delta3;
+    T m_detH;
+    
+    T m_delta1_tilda;
+    T m_delta2_tilda;
+    T m_delta3_tilda;
+    T m_detH_tilda;
+    
+    //Result of third covariant
+    T m_Aj;
+    T m_Bj;
+    T m_Cj;
+    T m_Dj;
+    
+    
+    
+    //---Functions for Constructor------
+    void first_covariant()
+    {
+        Vector4T<T> coefs;
+        coefs << m_A, m_B, m_C, m_D;
+        
+        Matrix4T<T> T_big;
+        
+        T_big(0, 0) = std::pow(m_t, 3);
+        T_big(0, 1) = static_cast<T>(3.0) * m_u * m_t2;
+        T_big(0, 2) = static_cast<T>(3.0) * m_t * m_u2;
+        T_big(0, 3) = std::pow(m_u, 3);
+        
+        T_big(1, 0) = m_t2 * m_s;
+        T_big(1, 1) = pr_product_difference<T>(static_cast<T>(2.0) * m_t, m_u * m_s, -m_t2, m_v);
+        T_big(1, 2) = pr_product_difference<T>(static_cast<T>(2.0) * m_t, m_u * m_v, -m_u2, m_s);
+        T_big(1, 3) = m_u2 * m_v;
+        
+        T_big(2, 0) = m_t2 * m_s
+        T_big(2, 1) = pr_product_difference<T>(static_cast<T>(2.0) * m_t, m_s * m_v, -m_s2, m_u);
+        T_big(2, 2) = pr_product_difference<T>(static_cast<T>(2.0) * m_u, m_s * m_v, -m_v2, m_t);
+        T_big(2, 3) = m_u2 * m_v
+            
+        T_big(3, 0) = std::pow(m_s, 3);
+        T_big(3, 1) = static_cast<T>(3.0) * m_v * m_s2;
+        T_big(3, 2) = static_cast<T>(3.0) * m_s * m_v2;
+        T_big(3, 3) = std::pow(m_v, 3);
+        
+        m_coefs = T_big * coefs;
+    }
+    
+    void second_covariant()
+    {
+        T coef = std::pow(pr_product_difference<T>(m_t, m_v, m_s, m_u), 2);
+        
+        matrix2T<T> mat_params;
+        mat_params << m_t, m_u, m_s, m_v;
+        
+        //---Init mat_delta---
+        m_delta1 = pr_product_difference<T>(m_A, m_C, m_B, m_B);
+        m_delta2 = pr_product_difference<T>(m_A, m_D, m_B, m_C);
+        m_delta3 = pr_product_difference<T>(m_B, m_D, m_C, m_C);
+        m_detH   = pr_product_difference<T>(4 * delta1, delta3, delta2, delta2);
+        
+        matrix2T<T> mat_delta;
+        mat_delta << static_cast<T>(2.0) * delta1, delta2, delta2, static_cast<T>(2.0) * delta3;
+        
+        //Calc delta tilda
+        matrix2T<T> mat_delta_tilda;
+        mat_delta_tilda = coef * mat_params * mat_delta * mat_params.transpose();
+        
+        m_delta1_tilda = mat_delta_tilda(0,0) / static_cast<T>(2.0);
+        m_delta2_tilda = mat_delta_tilda(0,1);
+        m_delta3_tilda = mat_delta_tilda(1,1) / static_cast<T>(2.0);
+        m_detH_tilda = coef * m_detH; 
+    }
+    
+    void third_covariant()
+    {
+        m_Aj = pr_product_difference<T>(std::pow(m_A, 2), m_D, static_cast<T>(3.0)* m_A, m_B * m_C) 
+               + static_cast<T>(2.0) * std::pow(m_B, 3);   
+        m_Bj = pr_product_difference<T>(std::pow(m_B, 2), m_C, static_cast<T>(2.0), m_A * std::pow(m_C, 2)) + (m_A * m_B * m_D) 
+        m_Cj = pr_product_difference<T>(std::pow(m_B, 2), static_cast<T>(2.0) * m_D, m_B, std::pow(m_C, 2)) - (m_A * m_C * m_D) 
+        m_Dj = pr_product_difference<T>(static_cast<T>(3.0) * m_B, m_C * m_D, static_cast<T>(2.0), std::pow(m_C, 3)) 
+            - (m_A * std::pow(m_D, 2))
+            
+    }
+};
+
+template<typename T>
+std::pair<T, T> testPolynomial(unsigned int roots_count)
+{
+    T max_absolute_error, max_relative_error; 
+    
+    std::vector<T> roots_computed(roots_count); 
+    std::vector<T> roots(roots_count)
+    std::vector<T> coefficients(roots_count + 1); 
+    generate_polynomial<T>(roots_count, 0, roots_count, 0, 1e-10, -1.0, 1.0, roots, coefficients);
+ 
+    Blinn solver(coefficients[3], coefficients[2], coefficients[1], coefficients[0], t, u, 1);
+    
+    int cnt_real_roots = solver.solve(roots_computed); // находим число вещ.корней и сами корни
+    if (cnt_real_roots != 0 && cnt_real_roots != -1)
+    { 
+        //если корни найдены и мы не попали в какой-то исключ.случай, то сравниваем найденные корни с истинными
+        compare_roots<fp_t>(roots_computed.size(), roots.size(), roots_computed, roots, max_absolute_error, max_relative_error);
+         
+    } 
     else
     {
-        //---L---
-        T CbarA = d1;
-        T DbarA = pr_product_difference<T>(A, d2, 2 * B, d1);
-        T thetaA = fabs(atan2(A *sqrt(det), -DbarA)) / 3.0;
-        T xt1A = 2 * sqrt(-CbarA) * cos(thetaA);
-        //xt3A = 2 * sqrt(-CbarA) * (cos(thetaA) / -2. - (sqrt(3.) / 2.)*sin(thetaA));
-        T xt3A =  pr_product_difference<T>(-sqrt(-CbarA * 3.0), sin(thetaA), sqrt(-CbarA), cos(thetaA));
-        T xtL = (xt1A + xt3A > 2 * B)? xt1A : xt3A;
-        
-        roots.push_back({xtL - B, A});
-        
-        //---S---
-        T CbarD = d3;
-        T DbarD = pr_product_difference<T>(2 * C, d3, D, d2);
-        T thetaD = fabs(atan2(D * sqrt(det), -DbarD)) / 3.0;
-        T xt1D = 2 * sqrt(-CbarD) * cos(thetaD);
-        //T xt3D = 2 * sqrt(-CbarD) * (cos(thetaD) / -2.0 - (sqrt(3.0) / 2.0) *sin(thetaD));
-        T xt3D = pr_product_difference<T>(-sqrt(-CbarD), cos(thetaD), sqrt(-CbarD * 3.0), sin(thetaD)); 
-                     
-        T xtS = (xt1D + xt3D < 2 * C)? xt1D : xt3D;
-        roots.push_back({ -D, xtS + C });
-                     
-        //---M---
-        T E = roots[0][1] * roots[1][1];
-        T F = pr_product_difference<T>(-roots[0][0], roots[1][1], roots[0][1], roots[1][0]);
-        T G = roots[0][0] * roots[1][0];
-
-        roots.push_back({pr_product_difference<T>(C, F, B, G),  pr_product_difference<T>(C, E, B, F)});
-
+        max_absolute_error = 0;
+        max_relative_error = 0;
     }
-    return roots;
+    
+    return std::pair<T, T>(max_absolute_error, max_relative_error);
 }
 
 int main()
@@ -104,46 +202,27 @@ int main()
       std::cout << "FMA disabled\n";
     #endif
     
+  
+    solver.solve();
     
-    unsigned int p = 3;
-    std::vector<double> roots(p);
-    std::vector<double> coefs(p + 1);
+    float max_absolut_deviation = 0;
+    float max_relative_deviation = 0;
     
-    int code = generate_polynomial<double>(p, 0, 2, 0, 1e-5, 0.0, 1.0, roots, coefs);
-    if (code < 0)
+    for (auto i = 0; i < 10'000'000; ++i) 
     {
-        std::cout << "Error in generate_polynomial\n";
-    }
-    
-    std::vector<std::vector<double>> my_roots = blinn(coefs[3],coefs[2],coefs[1], coefs[0]);
-    std::vector<double> norm_roots;
-    for (auto v : my_roots)
-    {
-        norm_roots.push_back(v[0] / v[1]);
+        auto deviation = testPolynomial<float>(3);
         
+        if (deviation.first > max_absolut_deviation) 
+        {
+            max_absolut_deviation = deviation.first;
+        }
+        if (deviation.second > max_relative_deviation) 
+        {
+            max_relative_deviation = deviation.second;
+        }
     }
-
-    double max_abs = 0.0;
-    double max_rel = 0.0;
-    int rv = compare_roots<double>(3, 3, norm_roots, roots, max_abs, max_rel);
     
-    //---Output---
-    std::cout << "max_abs: " << max_abs << '\n';
-    std::cout << "max_rel: " << max_rel << '\n';
-    std::cout << "rv: " << rv << '\n';
-
-    std::cout << "Roots: ";
-    for(auto elem : roots)
-    {
-        std::cout << elem <<  " ";
-    }
-    std::cout << "\n";
-    
-    std::cout << "My_roots: ";
-    for(auto elem : norm_roots)
-    {
-        std::cout << elem <<  " ";   
-    }
-    std::cout << "\n";
+    std::cout << endl << "MAX_ABSOLUT_deviation = " << max_absolut_deviation << endl;
+    std::cout << endl << "MAX_RELATIVE_deviation = " << max_relative_deviation << endl;
     
 }
